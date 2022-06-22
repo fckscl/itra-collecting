54471563c8754d827e655018f9ebd9f7882bcd86
fckscl <dimapugach31102003@gmail.com>
2022-06-22 16:57:58 +0300

    Добавьте файлы проекта.

---
+++ Kyrs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kyrs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
